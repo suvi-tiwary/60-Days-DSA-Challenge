e181d43ccecbc61e971990f1db109c29fc0b4b58
suvi-tiwary <suvikumari345@gmail.com>
2025-11-22 10:35:19 +0530

    1 . Binary search

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 using namespace std;
  
-def BinarySearch ( int arr( int n),int tar){
+int BinarySearch ( int arr[],int n,int tar){
     int st=0,end=n-1;
     while(st<=end){
-       int mid= st+ (end-st)/2;
+      int mid= st+ (end-st)/2;
         if(tar==arr[mid]){
-            return mid
+            return mid;
         }
-        elseif(tar>arr[mid]){
+        else if(tar>arr[mid]){
             st=mid+1;
         }
         else{
             end=mid-1;
         }
     }
+    return -1;
 }
 int main (){
-   arr[5]=[2,3,4,5,6,6,9]
-   BinarySearch(arr,9)
+  int arr[]={2,3,4,5,6};
+ int result = BinarySearch(arr,5,2);
+   cout << result;
 
     return 0;
 }
